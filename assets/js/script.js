c633dc8f85d3a19ce2c7a1a52d821331ade5354b
Adam-Smart <adams8632@hotmail.co.uk>
2023-05-21 21:12:16 +0100

    added icons for other days of the week

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,13 +57,6 @@ const lookupLocation = (search) => {
 var displayWeather = (weatherData) => {
     
     var currentWeather = weatherData.current
-
-    document.getElementById("temp-val").textContent =`${currentWeather.temp}°C`
-    document.getElementById("wind-val").textContent = `${currentWeather.wind_speed}Mph`
-    document.getElementById("humidity-val").textContent = `${currentWeather.humidity}%`
-
-    console.log (currentWeather)
-
     var weatherIcon = document.getElementById('weather-icon');
     weatherIcon.innerHTML = '';
 
@@ -71,67 +64,95 @@ var displayWeather = (weatherData) => {
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png"/>`
     weatherIcon.appendChild(img);
 
+    document.getElementById("temp-val").textContent =`${currentWeather.temp}°C`
+    document.getElementById("wind-val").textContent = `${currentWeather.wind_speed}Mph`
+    document.getElementById("humidity-val").textContent = `${currentWeather.humidity}%`
+
+    console.log (currentWeather)
+
+   
 }
 var displayForecast = (weatherData) => {
     var dailyData = (weatherData.daily)
 
-     var dailyForecast = dailyData[1];
-     var day1 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
-
- console.log(day1)
-
-    $('#day1-val').text(day1);
+var dailyForecast = dailyData[1];
+var day1 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
+$('#day1-val').text(day1);
+    
     var weatherForecast = weatherData.daily[0]
+    var weatherIcon = document.getElementById('day1-weather-icon');
+     weatherIcon.innerHTML = '';
+    var img = document.createElement("div");
+    img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[1].weather[0].icon}.png"/>`
+    weatherIcon.appendChild(img);
+
     document.getElementById("day1temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day1wind-val").textContent = `${weatherForecast.wind_speed}Mph`
     document.getElementById("day1humidity-val").textContent = `${weatherForecast.humidity}%`
 
-    console.log (dailyData)
+     var dailyForecast = dailyData[2];
+ var day2 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
+$('#day2-val').text(day2);
 
-    var dailyForecast = dailyData[2];
-     var day2 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
-
-    $('#day2-val').text(day2);
     var weatherForecast = weatherData.daily[1]
+    var weatherIcon = document.getElementById('day2-weather-icon');
+     weatherIcon.innerHTML = '';
+    var img = document.createElement("div");
+    img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[2].weather[0].icon}.png"/>`
+    weatherIcon.appendChild(img);
+
     document.getElementById("day2temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day2wind-val").textContent = `${weatherForecast.wind_speed}Mph`
     document.getElementById("day2humidity-val").textContent = `${weatherForecast.humidity}%`
 
-    var dailyForecast = dailyData[3];
-     var day3 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
-
+var dailyForecast = dailyData[3];
+var day3 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
+$('#day3-val').text(day3);
+    
     var weatherForecast = weatherData.daily[2]
+    var weatherIcon = document.getElementById('day3-weather-icon');
+    weatherIcon.innerHTML = '';
+    var img = document.createElement("div");
+    img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[3].weather[0].icon}.png"/>`
+    weatherIcon.appendChild(img);
 
-    $('#day3-val').text(day3);
     document.getElementById("day3temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day3wind-val").textContent = `${weatherForecast.wind_speed}Mph`
     document.getElementById("day3humidity-val").textContent = `${weatherForecast.humidity}%`
 
-    var dailyForecast = dailyData[4];
+     var dailyForecast = dailyData[4];
      var day4 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
-
+$('#day4-val').text(day4);
+      
     var weatherForecast = weatherData.daily[3]
-    
-    $('#day4-val').text(day4);
+    var weatherIcon = document.getElementById('day4-weather-icon');
+     weatherIcon.innerHTML = '';
+    var img = document.createElement("div");
+    img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[4].weather[0].icon}.png"/>`
+    weatherIcon.appendChild(img);
+
     document.getElementById("day4temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day4wind-val").textContent = `${weatherForecast.wind_speed}Mph`
     document.getElementById("day4humidity-val").textContent = `${weatherForecast.humidity}%`
+    console.log (day4)
 
     var dailyForecast = dailyData[5];
-     var day5 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
-
-    var weatherForecast = weatherData.daily[4]
+    var day5 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'})
+$('#day5-val').text(day5);
     
-    $('#day5-val').text(day5);
+    var weatherForecast = weatherData.daily[4]
+    var weatherIcon = document.getElementById('day5-weather-icon');
+     weatherIcon.innerHTML = '';
+    var img = document.createElement("div");
+    img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[5].weather[0].icon}.png"/>`
+    weatherIcon.appendChild(img);
+
     document.getElementById("day5temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day5wind-val").textContent = `${weatherForecast.wind_speed}Mph`
     document.getElementById("day5humidity-val").textContent = `${weatherForecast.humidity}%`
     
    
 }
-
-
-
 
 function loadLocation(){
     const storedLocations = JSON.parse(localStorage.getItem("recentLocations"));
@@ -152,7 +173,6 @@ console.log(recentLocations[i])
     }
     
 }
-
 
 function saveLocationOnClick (event){
     console.log ("This will save location")
