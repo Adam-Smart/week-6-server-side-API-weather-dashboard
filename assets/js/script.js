1792c9c3e7e29a79411338e449132c5f7fe541ab
Adam-Smart <adams8632@hotmail.co.uk>
2023-05-22 23:29:06 +0100

    organized code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
-const WEATHER_API_KEY = '3770aa61038a0816864d556d797ecb9f';
-const recentLocations = []
+const Weather_URL = 'https://api.openweathermap.org';
+const Weather_Key = '3770aa61038a0816864d556d797ecb9f';
+const history_location = []
 var locationInput = $("#location")
 var getLocation = () => {
-    var userLocation = locationInput.value;
-    if (userLocation === "") {
+    var userInput = locationInput.value;
+    if (userInput === "") {
         locationError();
     } else {
-        lookupLocation(userLocation)
+        searchPlace(userInput)
     }
 }
-// Shows error message if no loacion is entered 
+// Shows error message if no locaion is entered 
 function locationError (){ 
 var errorMsg = "Enter Valid Location:"
-var messageDiv =document.getElementById("searchError")
+var messageDiv = document.getElementById("searchError")
 messageDiv.textContent = errorMsg
     // Hides the error message after 2 seconds 
     setTimeout(function(){
@@ -22,15 +22,15 @@ messageDiv.textContent = errorMsg
     }
 
 
-// creates the array for searched locations
+// Creates the array for searched locations
 
-const lookupLocation = (search) => {
+const searchPlace = (search) => {
 //Saves the searches into the local storage 
     saveLocation(search);
 
     // Finds the location using the lat/lon
-    var apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
-    fetch(apiUrl)
+    var APIurl = `${Weather_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${Weather_Key}`;
+    fetch(APIurl)
         .then(response => response.json())
         .then(data => {
 
@@ -45,9 +45,9 @@ const lookupLocation = (search) => {
 
             //Gets the weather for the location
             
-            var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
-            console.log(apiUrl);
-            fetch(apiUrl)
+            var APIurl = `${Weather_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=${Weather_Key}`;
+            console.log(APIurl);
+            fetch(APIurl)
                 .then(response => response.json())
                 .then(data => {
 
@@ -67,11 +67,11 @@ var displayWeather = (weatherData) => {
     
     var currentWeather = weatherData.current
     // Shows icon
-    var weatherIcon = document.getElementById('weather-icon');
-    weatherIcon.innerHTML = '';
+    var icon = document.getElementById('weather-icon');
+    icon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png"/>`
-    weatherIcon.appendChild(img);
+    icon.appendChild(img);
 
     document.getElementById("temp-val").textContent =`${currentWeather.temp}°C`
     document.getElementById("wind-val").textContent = `${currentWeather.wind_speed}Mph`
@@ -90,11 +90,11 @@ var day1 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekd
 $('#day1-val').text(day1);
     
     var weatherForecast = weatherData.daily[0]
-    var weatherIcon = document.getElementById('day1-weather-icon');
-     weatherIcon.innerHTML = '';
+    var icon = document.getElementById('day1-weather-icon');
+     icon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[1].weather[0].icon}.png"/>`
-    weatherIcon.appendChild(img);
+    icon.appendChild(img);
 
     document.getElementById("day1temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day1wind-val").textContent = `${weatherForecast.wind_speed}Mph`
@@ -105,11 +105,11 @@ $('#day1-val').text(day1);
 $('#day2-val').text(day2);
 
     var weatherForecast = weatherData.daily[1]
-    var weatherIcon = document.getElementById('day2-weather-icon');
-     weatherIcon.innerHTML = '';
+    var icon = document.getElementById('day2-weather-icon');
+     icon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[2].weather[0].icon}.png"/>`
-    weatherIcon.appendChild(img);
+    icon.appendChild(img);
 
     document.getElementById("day2temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day2wind-val").textContent = `${weatherForecast.wind_speed}Mph`
@@ -120,11 +120,11 @@ var day3 = new Date(dailyForecast.dt * 1000).toLocaleDateString('en-GB', { weekd
 $('#day3-val').text(day3);
     
     var weatherForecast = weatherData.daily[2]
-    var weatherIcon = document.getElementById('day3-weather-icon');
-    weatherIcon.innerHTML = '';
+    var icon = document.getElementById('day3-weather-icon');
+    icon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[3].weather[0].icon}.png"/>`
-    weatherIcon.appendChild(img);
+    icon.appendChild(img);
 
     document.getElementById("day3temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day3wind-val").textContent = `${weatherForecast.wind_speed}Mph`
@@ -135,11 +135,11 @@ $('#day3-val').text(day3);
 $('#day4-val').text(day4);
       
     var weatherForecast = weatherData.daily[3]
-    var weatherIcon = document.getElementById('day4-weather-icon');
-     weatherIcon.innerHTML = '';
+    var icon = document.getElementById('day4-weather-icon');
+     icon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[4].weather[0].icon}.png"/>`
-    weatherIcon.appendChild(img);
+    icon.appendChild(img);
 
     document.getElementById("day4temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day4wind-val").textContent = `${weatherForecast.wind_speed}Mph`
@@ -151,11 +151,11 @@ $('#day4-val').text(day4);
 $('#day5-val').text(day5);
     
     var weatherForecast = weatherData.daily[4]
-    var weatherIcon = document.getElementById('day5-weather-icon');
-     weatherIcon.innerHTML = '';
+    var icon = document.getElementById('day5-weather-icon');
+     icon.innerHTML = '';
     var img = document.createElement("div");
     img.innerHTML = ` <img src="https://openweathermap.org/img/w/${dailyData[5].weather[0].icon}.png"/>`
-    weatherIcon.appendChild(img);
+    icon.appendChild(img);
 
     document.getElementById("day5temp-val").textContent = `${weatherForecast.temp.day}°C`
     document.getElementById("day5wind-val").textContent = `${weatherForecast.wind_speed}Mph`
@@ -165,40 +165,38 @@ $('#day5-val').text(day5);
 }
 // Gets the locations from local storage
 function loadLocation(){
-    const storedLocations = JSON.parse(localStorage.getItem("recentLocations"));
+    const storeData = JSON.parse(localStorage.getItem("history_location"));
 
-    if (storedLocations !== null) {
-        recentLocations.push(...storedLocations);
+    if (storeData !== null) {
+        history_location.push(...storeData);
 
-        for (let i = 0; i < recentLocations.length; i++){
-            var newLocation = document.createElement("div");
-            newLocation.classList.add("recent-location");
-            newLocation.textContent = recentLocations[i];
-            newLocation.addEventListener("click",saveLocationOnClick);
+        for (let i = 0; i < history_location.length; i++){
+            var addedLocation = document.createElement("div");
+            addedLocation.classList.add("recent-location");
+            addedLocation.textContent = history_location[i];
+            addedLocation.addEventListener("click",saveLocationOnClick);
 
-            document.getElementById("recent-locations").appendChild(newLocation)
-console.log(newLocation)
-console.log(recentLocations[i])
+            document.getElementById("recent").appendChild(addedLocation)
+console.log(addedLocation)
+console.log(history_location[i])
         }
     }
     
 }
 // Function for when you click it saves location
 function saveLocationOnClick (event){
-    console.log ("This will save location")
-
     const location = event.target.textContent;
-    lookupLocation(location);
+    searchPlace(location);
 
 }
 // Saves location and adds it to local storage array and to recent location list
 function saveLocation(location){
-    const index = recentLocations.indexOf(location);
+    const index = history_location.indexOf(location);
 
     if (index === -1){
-        recentLocations.push(location);
+        history_location.push(location);
     
-        localStorage.setItem("recentLocations", JSON.stringify(recentLocations))
+        localStorage.setItem("history_location", JSON.stringify(history_location))
     }
 
 }
